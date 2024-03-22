c5921e0cedbff9efe8929858ff42a7f8985e3765
kyoya0819 <kyoya0819@gmail.com>
2024-03-22 16:16:36 +0900

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import type { LinksFunction } from "@remix-run/cloudflare";
+import {
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration
+} from "@remix-run/react";
+import React from "react";
+
+import cssHref from "./styles/app.css?url";
+
+export const links: LinksFunction = () => [
+    { rel: "stylesheet", href: cssHref }
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration/>
+                <script
+                    src="https://challenges.cloudflare.com/turnstile/v0/api.js?"
+                />
+                <Scripts/>
+            </body>
+        </html>
+    );
+}
+
+export default function App() {
+    return <Outlet/>;
+}
